be7437ee3a5ce4d94a853c8fb527d8f956f843c9
Jake Stephens <jake.stephens9@gmail.com>
2014-03-25 10:43:49 -0400

    Footstep sound effects in

---
+++ Assets/player/FootstepSfxHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepSfxHandler : MonoBehaviour 
+{
+
+	public AudioSource[] footsteps;
+
+	void start()
+	{
+		
+	}
+
+	void Update() 
+	{
+		if(this.GetComponent<CharacterController>().velocity.magnitude > 0.5f && this.GetComponent<CharacterController>().isGrounded)
+		{
+			playRandomFootstep();
+		}
+	}
+	
+	private void playRandomFootstep()
+	{
+		bool isPlaying = false;
+		for(int x = 0; x < footsteps.Length; x++)
+		{
+			if(footsteps[x].isPlaying)
+			{
+				isPlaying = true;
+			}
+		}
+		
+		if(!isPlaying)
+		{
+			footsteps[Random.Range(0, footsteps.Length)].Play();
+		}
+	}
+}
